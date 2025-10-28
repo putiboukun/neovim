b4f86781ee2ab3ca814d72b4d9311ed5f0a283f2
obito_od <94419003+putiboukun@users.noreply.github.com>
2025-10-28 09:46:43 +0900

    Create autopairs.lua

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,8 @@
+-- ~/.config/nvim/lua/plugins/autopairs.lua
+return {
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
+  config = function()
+    require("nvim-autopairs").setup({})
+  end,
+}
